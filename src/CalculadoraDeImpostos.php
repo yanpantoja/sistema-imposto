334b02f28b2041c2bc605a50d359cc51d693ab6d
Yan Pantoja <yanmpantoja@gmail.com>
2020-09-04 19:23:56 -0300

    feat: ifs removed with Strategy

---
+++ src/CalculadoraDeImpostos.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\Impostos\Imposto;
+
+class CalculadoraDeImpostos
+{
+
+    public function calcula(Orcamento $orcamento, Imposto $imposto): float
+    {
+        return $imposto->calculaImposto($orcamento);
+    }
+}
---
+++ src/Orcamento.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+class Orcamento
+{
+    public float $valor;
+}
