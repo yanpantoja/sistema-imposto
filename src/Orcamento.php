f7203296db83c7225e799f504e9180b04196b60e
Yan Pantoja <yanmpantoja@gmail.com>
2020-09-09 19:04:53 -0300

    feat: State implemented

--- src/Orcamento.php
+++ src/Orcamento.php
@@ -2,8 +2,37 @@
 
 namespace Alura\DesignPattern;
 
+use Alura\DesignPattern\EstadosOrcamento\EmAprovacao;
+use Alura\DesignPattern\EstadosOrcamento\EstadoOrcamento;
+
 class Orcamento
 {
     public int $quantidadeItems;
     public float $valor;
+    public EstadoOrcamento $estadoAtual;
+
+    public function __construct()
+    {
+        $this->estadoAtual = new EmAprovacao();
+    }
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->estadoAtual->calculaDescontoExtra($this);
+    }
+
+    public function aprova()
+    {
+        $this->estadoAtual->aprova($this);
+    }
+
+    public function reprova()
+    {
+        $this->estadoAtual->reprova($this);
+    }
+
+    public function finaliza()
+    {
+        $this->estadoAtual->finaliza($this);
+    }
 }
